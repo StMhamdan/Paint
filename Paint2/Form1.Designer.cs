e6f38e89c5dde73f856c59029507df8b53f97c5c
عبد الرحمن طباسي <AC1394@iu.edu.jo>
2022-02-05 17:51:52 +0200

    Add project files.

---
+++ Paint2/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace Paint2
+{
+    partial class PaintPr
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelPaint = new System.Windows.Forms.Panel();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.btnPen = new System.Windows.Forms.Button();
+            this.pictureBoxHome = new System.Windows.Forms.PictureBox();
+            this.PanelPaint.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHome)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PanelPaint
+            // 
+            this.PanelPaint.BackColor = System.Drawing.Color.Aqua;
+            this.PanelPaint.Controls.Add(this.btnRectangle);
+            this.PanelPaint.Controls.Add(this.btnCircle);
+            this.PanelPaint.Controls.Add(this.btnPen);
+            this.PanelPaint.Dock = System.Windows.Forms.DockStyle.Right;
+            this.PanelPaint.Location = new System.Drawing.Point(685, 0);
+            this.PanelPaint.Name = "PanelPaint";
+            this.PanelPaint.Size = new System.Drawing.Size(115, 450);
+            this.PanelPaint.TabIndex = 0;
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.BackColor = System.Drawing.Color.Red;
+            this.btnRectangle.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRectangle.Location = new System.Drawing.Point(15, 290);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(97, 77);
+            this.btnRectangle.TabIndex = 2;
+            this.btnRectangle.Text = "Rectangle";
+            this.btnRectangle.UseVisualStyleBackColor = false;
+            this.btnRectangle.Click += new System.EventHandler(this.btnRectangle_Click);
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.BackColor = System.Drawing.Color.Red;
+            this.btnCircle.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCircle.Location = new System.Drawing.Point(19, 150);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(75, 77);
+            this.btnCircle.TabIndex = 1;
+            this.btnCircle.Text = "Circle";
+            this.btnCircle.UseVisualStyleBackColor = false;
+            this.btnCircle.Click += new System.EventHandler(this.btnCircle_Click);
+            // 
+            // btnPen
+            // 
+            this.btnPen.BackColor = System.Drawing.Color.Red;
+            this.btnPen.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPen.Location = new System.Drawing.Point(19, 12);
+            this.btnPen.Name = "btnPen";
+            this.btnPen.Size = new System.Drawing.Size(75, 77);
+            this.btnPen.TabIndex = 0;
+            this.btnPen.Text = "Pen";
+            this.btnPen.UseVisualStyleBackColor = false;
+            this.btnPen.Click += new System.EventHandler(this.btnPen_Click);
+            // 
+            // pictureBoxHome
+            // 
+            this.pictureBoxHome.BackColor = System.Drawing.Color.White;
+            this.pictureBoxHome.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxHome.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxHome.Name = "pictureBoxHome";
+            this.pictureBoxHome.Size = new System.Drawing.Size(685, 450);
+            this.pictureBoxHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxHome.TabIndex = 1;
+            this.pictureBoxHome.TabStop = false;
+            this.pictureBoxHome.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxHome_Paint);
+            this.pictureBoxHome.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxHome_MouseDown);
+            this.pictureBoxHome.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxHome_MouseMove);
+            this.pictureBoxHome.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBoxHome_MouseUp);
+            // 
+            // PaintPr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBoxHome);
+            this.Controls.Add(this.PanelPaint);
+            this.Name = "PaintPr";
+            this.Text = "Paint";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.PanelPaint.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHome)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelPaint;
+        private System.Windows.Forms.Button btnPen;
+        private System.Windows.Forms.PictureBox pictureBoxHome;
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.Button btnRectangle;
+    }
+}
+
---
+++ Paint2/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint2
+{
+    public partial class PaintPr : Form
+    {
+        public PaintPr()
+        {
+            InitializeComponent();
+            bm = new Bitmap(pictureBoxHome.Width, pictureBoxHome.Height);
+            g = Graphics.FromImage(bm);
+            g.Clear(Color.White);
+            pictureBoxHome.Image = bm;
+
+        }
+
+        Bitmap bm;
+        Graphics g;
+        bool paint = false;
+        Point px, py;
+        Pen p = new Pen(Color.Black, 1);
+        Pen erase = new Pen(Color.White, 10);
+        int select;
+        int x, y, sx, sy, cx, cy;
+
+        private void btnRectangle_Click(object sender, EventArgs e)
+        {
+            select = 3;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBoxHome_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            if (paint)
+            {
+                if (select == 2)
+                {
+                    g.DrawEllipse(p, cx, cy, sx, sy);
+                }
+                if (select == 3)
+                {
+                    g.DrawRectangle(p, cx, cy, sx, sy);
+                }
+               
+
+            }
+        }
+
+        private void btnCircle_Click(object sender, EventArgs e)
+        {
+            select = 2;
+        }
+
+        private void pictureBoxHome_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint = true;
+            py = e.Location;
+          
+            cx = e.X;
+            cy = e.Y;
+        }
+
+        private void pictureBoxHome_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (paint)
+            {
+                if (select == 1)
+                {
+                    px = e.Location;
+                    g.DrawLine(p, px, py);
+                    py = px;
+                }
+                if (select == 2)
+                {
+                    px = e.Location;
+                    g.DrawLine(erase, px, py);
+                    py = px;
+                }
+                pictureBoxHome.Refresh();
+                x = e.X;
+                y = e.Y;
+                sx = e.X - cx;
+                sy = e.Y - cy;
+            }
+        }
+
+        private void pictureBoxHome_MouseUp(object sender, MouseEventArgs e)
+        {
+            paint = false;
+            sx = x - cx;
+            sy = y - cy;
+            if (select == 2)
+            {
+                g.DrawEllipse(p, cx, cy, sx, sy);
+            }
+            if (select == 3)
+            {
+                g.DrawRectangle(p, cx, cy, sx, sy);
+            }
+        }
+
+        private void btnPen_Click(object sender, EventArgs e)
+        {
+            select = 1;
+        }
+    }
+}
